9b91477b4f387e70ce736d0a8a753bc821c0fc9a
Li Guoteng <liguoteng2017@outlook.com>
2023-05-29 16:53:12 +0800

    3.接入日志框架logback

--- mypush-web/src/main/java/com/ltgds/mypush/web/TestController.java
+++ mypush-web/src/main/java/com/ltgds/mypush/web/TestController.java
@@ -1,5 +1,6 @@
 package com.ltgds.mypush.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping("/test")
     private String test() {
+        System.out.println("测试logback");
+        log.info("测试logback11111");
         return "hello";
     }
 }
